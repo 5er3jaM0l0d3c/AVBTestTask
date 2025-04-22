46fc82ba99625c67f22eca3419f9e44da87d5f21
5er3jaM0l0d3c <telephonecizerwed@gmail.com>
2025-04-22 13:40:37 +0300

    productService

---
+++ ProductAPI/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductEntities;
+using ProductServices.Interface;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProduct Product { get; set; }
+
+        public ProductsController(IProduct product)
+        {
+            this.Product = product;
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public Product? GetProducts(int id)
+        {
+            return Product.GetProduct(id);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult AddProduct([FromBody] Product product) 
+        {
+            Product.AddProduct(product);
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPut("{productId}/stock")]
+        public IActionResult UpdateProduct(int productId, int amount)
+        {
+            Product.UpdateProduct(productId, amount);
+            return Ok();
+        }
+    }
+}
---
+++ ProductEntities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductEntities;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+    public int Amount { get; set; }
+}
