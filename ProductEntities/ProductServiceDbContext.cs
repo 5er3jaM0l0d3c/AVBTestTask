b82271dd163e20a92f35e19c53e306a26d73025c
5er3jaM0l0d3c <telephonecizerwed@gmail.com>
2025-04-22 12:57:19 +0300

    Create context

---
+++ ProductEntities/ProductServiceDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductEntities;
+
+public partial class ProductServiceDbContext : DbContext
+{
+    public ProductServiceDbContext()
+    {
+    }
+
+    public ProductServiceDbContext(DbContextOptions<ProductServiceDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Product { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ProductServiceDB;Username=postgres;Password=postgres");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("Product_pkey");
+
+            entity.ToTable("Product");
+
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Price).HasPrecision(8, 2);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
