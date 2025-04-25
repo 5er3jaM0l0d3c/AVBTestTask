0e944bee5e1b48224b0a986ea7818aa6761a4a33
5er3jaM0l0d3c <telephonecizerwed@gmail.com>
2025-04-25 09:56:24 +0300

    fix

--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using OrderEntities;
 using OrderServices.Interface;
+using OrderServices.Service;
 using ProductEntities;
 using System;
 
@@ -8,7 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<OrderServiceDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var dbHost = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true"
+    ? "db1"
+    : "localhost";
+
+// Строим строку подключения
+var connectionString = $"Server={dbHost};Port=5432;Database=OrderServiceDB;User Id=postgres;Password=postgres;";
+
+// Регистрируем контекст
+builder.Services.AddDbContext<OrderServiceDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IOrder, OrderServices.Service.OrderServices>();
 
@@ -19,6 +29,29 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OrderServiceDbContext>();
+
+    try
+    {
+        // Проверяем, есть ли pending миграции
+        var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+        if (pendingMigrations.Any())
+        {
+            Console.WriteLine($"Applying migrations: {string.Join(", ", pendingMigrations)}");
+            await dbContext.Database.MigrateAsync();
+            Console.WriteLine("Migrations applied successfully");
+        }
+        else
+        {
+            Console.WriteLine("No pending migrations to apply");
+        }
+    }
+    catch
+    {
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,4 +65,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 
-app.Run("http://*:80"); ;
+app.Run("http://*:82");
--- ProductEntities/ProductServiceDbContext.cs
+++ ProductEntities/ProductServiceDbContext.cs
@@ -17,10 +17,6 @@ public partial class ProductServiceDbContext : DbContext
 
     public virtual DbSet<Product> Product { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=ProductServiceDB;Username=postgres;Password=postgres");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Product>(entity =>
